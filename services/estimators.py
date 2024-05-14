602874b8b4717f984d8b29fa2aa7eb9ef77d9b54
kevin <kevinskw@outlook.com>
2024-05-14 19:27:36 -0400

    encapsulated the mu and Q calculation

--- services/estimators.py
+++ services/estimators.py
@@ -1,4 +1,5 @@
 import numpy as np
+from services.helper import cal_Q,cal_mu
 
 
 def OLS(returns, factRet):
@@ -18,25 +19,7 @@ def OLS(returns, factRet):
     # Regression coefficients
     B = np.linalg.solve(X.T @ X, X.T @ returns)
 
-    # Separate B into alpha and betas
-    a = B[0, :]
-    V = B[1:, :]
-
-    # Residual variance
     ep = returns - X @ B
-    sigma_ep = 1 / (T - p - 1) * np.sum(ep.pow(2), axis=0)
-    D = np.diag(sigma_ep)
-
-    # Factor expected returns and covariance matrix
-    f_bar = np.expand_dims(factRet.mean(axis=0).values, 1)
-    F = factRet.cov().values
-
-    # Calculate the asset expected returns and covariance matrix
-    mu = np.expand_dims(a, axis=1) + V.T @ f_bar
-    Q = V.T @ F @ V + D
-
-    # Sometimes quadprog shows a warning if the covariance matrix is not
-    # perfectly symmetric.
-    Q = (Q + Q.T) / 2
-
+    mu = cal_mu(B, factRet)
+    Q = cal_Q(B, factRet, ep)
     return mu, Q
---
+++ services/helper.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def cal_mu(beta, factRet):
+    a = beta[0, :]
+    V = beta[1:, :]
+    f_bar = np.expand_dims(factRet.mean(axis=0).values, 1)
+    mu = np.expand_dims(a, axis=1) + V.T @ f_bar
+    return mu
+
+def cal_Q(beta, factRet, residuals):
+    """compute the vcov matrix with formula Q = B'FB + delta"""
+    T = len(residuals)
+    tol = 1e-6
+    V = beta[1:, :]
+    p = np.apply_along_axis(lambda x: np.sum(np.abs(x) > tol), axis=0, arr=V)
+    F = factRet.cov().values
+    sigma_ep = 1 / (T - p - 1) * np.sum(residuals.pow(2), axis=0)
+    D = np.diag(sigma_ep)
+    Q = V.T @ F @ V + D
+    Q = (Q + Q.T) / 2
+    return Q
